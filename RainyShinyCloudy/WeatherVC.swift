25accce9638a10e8ff04acf1b5a5f1278c0ed5a2
Ken <kenaramaki@gmail.com>
2017-05-29 00:29:51 -0300

    table view, delegate and datasource

--- RainyShinyCloudy/WeatherVC.swift
+++ RainyShinyCloudy/WeatherVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // MARK: - Outlets
     @IBOutlet weak var dateLabel: UILabel!
@@ -17,17 +17,36 @@ class WeatherVC: UIViewController {
     @IBOutlet weak var currentWeatherImage: UIImageView!
     @IBOutlet weak var currentWeatherTypeLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
-    
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        tableView.delegate = self
+        tableView.dataSource = self
     
     }
 
-  
+    // MARK: - TableView Methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+        
+    }
+
+    // Setting the height of the cell to 80
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    
 }
 
