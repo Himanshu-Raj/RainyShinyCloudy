80f7938b329280f5c52e291f9dc743d588a1a4f0
Ken <kenaramaki@gmail.com>
2017-05-28 23:46:04 -0300

    created interface and outlets

---
+++ RainyShinyCloudy/WeatherVC.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherVC.swift
+//  RainyShinyCloudy
+//
+//  Created by Árthur Ken Aramaki Mota on 27/05/17.
+//  Copyright © 2017 Árthur Ken Aramaki Mota. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    
+    }
+
+  
+
+}
+
